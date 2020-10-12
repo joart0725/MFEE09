6403afdf5701c38f7528b1636ef77eb5a5928892
Anna <anna@joomla.com.tw>
2020-10-12 11:30:50 +0800

    Update App.js

--- 程式碼/1012/3.在App元件使用兩種不同的元件/App.js
+++ 程式碼/1012/3.在App元件使用兩種不同的元件/App.js
@@ -1,5 +1,5 @@
 // 導入其它的模組
-import React, { useState } from 'react'
+import React from 'react'
 
 // 導入要使用的元件程式，注意路徑要正確，不需要加副檔名
 import AppClass from './components/AppClass'
@@ -8,6 +8,7 @@ import AppFunction from './components/AppFunction'
 function App() {
   return (
     <>
+      {/* 使用類似HTML標記的方式來放置元件要呈現(渲染)的位置 */}
       <AppClass />
       <AppFunction />
     </>
