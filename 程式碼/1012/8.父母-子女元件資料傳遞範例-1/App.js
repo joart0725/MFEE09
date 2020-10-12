0701a85c3da074a82fff2a6bb4cc927959982eee
Anna <anna@joomla.com.tw>
2020-10-12 16:20:45 +0800

    add

---
+++ 程式碼/1012/8.父母-子女元件資料傳遞範例-1/App.js
@@ -0,0 +1,18 @@
+// 導入其它的模組
+import React, { useState } from 'react'
+
+import MyButtonOne from './components/MyButtonOne'
+import MyButtonTwo from './components/MyButtonTwo'
+
+function App(props) {
+  return (
+    <>
+      <h1>0</h1>
+      <MyButtonOne />
+      <MyButtonTwo />
+    </>
+  )
+}
+
+// 輸出元件(函式)
+export default App
---
+++ 程式碼/1012/8.父母-子女元件資料傳遞範例-1/components/MyButtonOne.js
@@ -0,0 +1,13 @@
+// 導入其它的模組
+import React, { useState } from 'react'
+
+function MyButtonOne(props) {
+  return (
+    <>
+      <button>One</button>
+    </>
+  )
+}
+
+// 輸出元件(函式)
+export default MyButtonOne
