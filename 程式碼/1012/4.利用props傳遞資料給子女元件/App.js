ab8202249e260f37105aeac17f49c54d84aa80c8
Anna <anna@joomla.com.tw>
2020-10-12 13:50:43 +0800

    ADD

---
+++ 程式碼/1012/4.利用props傳遞資料給子女元件/App.js
@@ -0,0 +1,20 @@
+// 導入其它的模組
+import React from 'react'
+
+// 導入要使用的元件程式，注意路徑要正確，不需要加副檔名
+import AppClass from './components/AppClass'
+import AppFunction from './components/AppFunction'
+
+function App() {
+  return (
+    <>
+      {/* 使用類似HTML標記的方式來放置元件要呈現(渲染)的位置 */}
+      {/* 使用類似HTML給定屬性值的方式來傳遞資料到子女元件中 */}
+      <AppClass title="類別型元件" initValue={99} />
+      <AppFunction title="函式型元件" initValue={101} />
+    </>
+  )
+}
+
+// 輸出元件(函式)
+export default App
---
+++ 程式碼/1012/4.利用props傳遞資料給子女元件/components/AppClass.js
@@ -0,0 +1,37 @@
+// 導入react函式庫
+import React from 'react'
+
+// 繼承類別
+class AppClass extends React.Component {
+  // 建構式
+  constructor(props) {
+    // 建構式的第一行需要使用super呼叫父母類別的建構式
+    super(props)
+    // 這裡建立狀態的初始化值
+    this.state = {
+      total: props.initValue,
+    }
+  }
+
+  // render的回傳值即為最後呈現在網頁上的元素
+  render() {
+    console.log(this.props)
+
+    return (
+      <>
+        {/* 呈現狀態值要使用this.state.total */}
+        <h1>{this.props.title}</h1>
+        <h1>{this.state.total}</h1>
+        {/* 設定/更動狀態要呼叫this.setState方法，給定之後狀態要改變的物件樣子 */}
+        <button onClick={() => this.setState({ total: this.state.total + 1 })}>
+          +1
+        </button>
+        <button onClick={() => this.setState({ total: this.state.total - 1 })}>
+          -1
+        </button>
+      </>
+    )
+  }
+}
+
+export default AppClass
