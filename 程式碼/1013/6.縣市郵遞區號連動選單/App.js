2016db89e401703fe9c37fe2814f3448b09a258c
Anna <anna@joomla.com.tw>
2020-10-13 14:10:23 +0800

    ADD

---
+++ 程式碼/1013/6.縣市郵遞區號連動選單/App.js
@@ -0,0 +1,14 @@
+// 導入其它的模組
+import React, { useState } from 'react'
+import TWZipCode from './homework2/TWZipCode'
+
+function App(props) {
+  return (
+    <>
+      <TWZipCode />
+    </>
+  )
+}
+
+// 輸出元件(函式)
+export default App
