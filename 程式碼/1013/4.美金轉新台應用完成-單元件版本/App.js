94ca090b2cbba079fd01c622e90a3dc104232143
Anna <anna@joomla.com.tw>
2020-10-13 11:19:55 +0800

    Update App.js

--- 程式碼/1013/4.美金轉新台應用完成-單元件版本/App.js
+++ 程式碼/1013/4.美金轉新台應用完成-單元件版本/App.js
@@ -5,6 +5,7 @@ function App(props) {
   const [twd, setTwd] = useState(0)
   const [usd, setUsd] = useState(0)
 
+  // 注意，這種數字運算會去掉小數然後四捨五入，所以不夠精準，只作範例用
   const usd2Twd = (usd) => (usd * 28.53).toFixed(2)
   const twd2Usd = (twd) => (twd > 0 ? (twd / 28.53).toFixed(2) : 0)
 
@@ -17,7 +18,7 @@ function App(props) {
         onChange={(e) => {
           // 先得到更動後的值
           const newTwd = e.target.value
-
+          // 再設定兩個需要設定的狀態值
           setTwd(newTwd)
           setUsd(twd2Usd(newTwd))
         }}
