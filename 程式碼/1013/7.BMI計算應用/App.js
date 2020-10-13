bce7eea4210360b69358c9b59d15a73bcc5ada8b
Anna <anna@joomla.com.tw>
2020-10-13 16:29:09 +0800

    add

---
+++ 程式碼/1013/7.BMI計算應用/App.js
@@ -0,0 +1,15 @@
+// 導入其它的模組
+import React, { useState } from 'react'
+import TWZipCode from './homework2/TWZipCode'
+import BMI from './homework/BMI'
+
+function App(props) {
+  return (
+    <>
+      <BMI />
+    </>
+  )
+}
+
+// 輸出元件(函式)
+export default App
