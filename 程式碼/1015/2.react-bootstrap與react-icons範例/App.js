52c4b5b70aa66de75cbbd99fd2ab43ca2578cf6a
Anna <anna@joomla.com.tw>
2020-10-15 11:00:05 +0800

    Create App.js

---
+++ 程式碼/1015/2.react-bootstrap與react-icons範例/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
+import { SiFacebook } from 'react-icons/si'
+
+function App() {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  return (
+    <>
+      <SiFacebook color="red" size={150} />
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default App
