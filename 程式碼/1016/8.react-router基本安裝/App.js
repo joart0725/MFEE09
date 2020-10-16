f37ae20aaa4d31e0e6c7b7e3d6696684979ea558
Anna <anna@joomla.com.tw>
2020-10-16 15:53:49 +0800

    add

---
+++ 程式碼/1016/8.react-router基本安裝/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+
+import MyNavbar from './components/MyNavbar'
+import MyFooter from './components/MyFooter'
+import MainContent from './components/MainContent'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+import TodoAppPage from './pages/TodoAppPage'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Router>
+      <>
+        <MyNavbar />
+        <MainContent>
+          {/* <Link to="/">Home</Link>
+          <Link to="/about">About</Link> */}
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/todo" component={TodoAppPage} />
+          </Switch>
+        </MainContent>
+        <MyFooter />
+      </>
+    </Router>
+  )
+}
+
+export default App
---
+++ 程式碼/1016/8.react-router基本安裝/pages/TodoAppPage.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+import TodoApp from '../components/TodoApp'
+
+function TodoAppPage(props) {
+  return <TodoApp />
+}
+
+export default TodoAppPage
