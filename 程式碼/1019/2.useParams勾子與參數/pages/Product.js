eab88643876da25c62d4fa907edacae7bff3e045
Anna <anna@joomla.com.tw>
2020-10-19 10:06:40 +0800

    add

---
+++ 程式碼/1019/2.useParams勾子與參數/pages/Product.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import { Link, Switch, useParams } from 'react-router-dom'
+
+function Product(props) {
+  let { id } = useParams()
+
+  console.log(props)
+  
+  return (
+    <>
+      <h1>Product</h1>
+      <h3>{id}</h3>
+    </>
+  )
+}
+
+export default Product
