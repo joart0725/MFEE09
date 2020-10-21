d531166ebd78313d3b7fb67eaa5c35ebcf4939e9
Anna <anna@joomla.com.tw>
2020-10-21 10:47:11 +0800

    UP

--- 程式碼/1021/2.Redux-拆分reducers與actions目錄/reducers/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-// 集中管理所有的動作類型
-
-// counter
-export const ADD_VALUE = 'ADD_VALUE'
-export const MINUS_VALUE = 'MINUS_VALUE'
--- 程式碼/1021/2.Redux-拆分reducers與actions目錄/reducers/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-
-import counter from './counter'
-
-// 合併所有的reducers成一個大的reducer
-export const rootReducer = combineReducers({
-  counter,
-})
