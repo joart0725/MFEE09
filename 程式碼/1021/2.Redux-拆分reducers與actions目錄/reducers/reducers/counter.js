426db749ad240151dbfb32eac3d8dd80daffbedc
Anna <anna@joomla.com.tw>
2020-10-21 10:42:41 +0800

    add

---
+++ 程式碼/1021/2.Redux-拆分reducers與actions目錄/reducers/reducers/counter.js
@@ -0,0 +1,14 @@
+import { ADD_VALUE, MINUS_VALUE } from '../actions/actionTypes'
+
+// action = { type: 'ADD_VALUE', value: 1 }
+// action = { type: 'MINUS_VALUE', value: 1 }
+export default function counter(state = 9999, action) {
+  switch (action.type) {
+    case ADD_VALUE:
+      return state + action.value
+    case MINUS_VALUE:
+      return state - action.value
+    default:
+      return state
+  }
+}
